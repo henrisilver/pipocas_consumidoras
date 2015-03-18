2260bc0db7c9a8e0e51cd87647e0e12b88096853
Henrique Silveira <hd2312@columbia.edu>
2015-03-18 20:13:56 -0300

    Modificacoes no trabalho: adicao de suporte a multiplas threads e adapatacao da funcao do produtor para multiplas threads, adicionando semaforo e modificando laco de repeticao.

--- prodcons_n_thread_sem.c
+++ prodcons_n_thread_sem.c
@@ -6,12 +6,11 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define MAX_PRODUCED 5000
-#define MAX_QUEUE 147
-#define MAX_PROD 10
-#define MAX_CONS 10
+#define MAX_PRODUCED 25
+#define MAX_QUEUE 5
+#define N 3
 
-sem_t  mutex, empty, full;
+sem_t mutex_critico, empty, full, mutex_produtor;
 
 int queue[MAX_QUEUE], item_available=0, produced=0, consumed=0;
 
@@ -54,31 +53,44 @@ void *producer(void) {
 	int item;
 
 	
-	while (produced < MAX_PRODUCED ) {
-	  item = create_item();
-	  sem_wait(&empty);
-	  sem_wait(&mutex);
-	  insert_into_queue(item);
-	  sem_post(&mutex);
-	  sem_post(&full);
+	while (1) {
+
+		sem_wait(&mutex_produtor);
+	// Condicao de checagem é movida para um if envolvido por um novo
+	// semaforo para evitar que um novo item seja produzido em excesso
+	// (caso haja uma preempcao de uma thread produtora e a substituta
+	// seja outra produtora, no momento em que o limite de itens foi
+	// atingido)
+		if(produced < MAX_PRODUCED) {
+			sem_wait(&empty);
+			sem_wait(&mutex_critico);
+	  		item = create_item(); // Producao de item envolvida por semaforos para
+	  						  // evitar que o numero maximo de itens
+	  						  // produzidos seja superado
+	  		insert_into_queue(item);
+	  		sem_post(&mutex_critico);
+	  		sem_post(&full);
+	  		sem_post(&mutex_produtor);
+	  }
+	  else {
+	  	sem_post(&mutex_produtor);
+	  	printf("\nThread producer saindo.\n\n");
+	  	fflush(0);
+	  	pthread_exit(0);
+	  }
 	}
-
-	printf("\nThread producer saindo.\n\n");
-	fflush(0);
-
-	pthread_exit(0);
 } // fim producer
 
 void *consumer(void) {
 	int my_item = 0;
 
 	while (consumed < MAX_PRODUCED) {
-	  sem_wait(&full);
-	  sem_wait(&mutex);
-	  my_item = extract_from_queue();
-	  sem_post(&mutex);
-	  sem_post(&empty);
-	  process_item(my_item);
+		sem_wait(&full);
+		sem_wait(&mutex_critico);
+		my_item = extract_from_queue();
+		sem_post(&mutex_critico);
+		sem_post(&empty);
+		process_item(my_item);
 	}
 
 	printf("\nThread consumer saindo.\n\n");
@@ -89,32 +101,35 @@ void *consumer(void) {
 
 
 int main(void) {
-	pthread_t prod_handle, cons_handle;
+	pthread_t prod_handle[N], cons_handle[N];
 
 	/* declarations and initializations */
-    	item_available = 0;
+	item_available = 0;
 
-	sem_init (&mutex, 0 , 1);
+	sem_init (&mutex_critico, 0 , 1);
 	sem_init(&empty, 0, MAX_QUEUE);
 	sem_init(&full, 0, 0);
+	sem_init (&mutex_produtor, 0 , 1);
 
 	/* create and join producer and consumer threads */
-
-	if (pthread_create(&prod_handle, 0, (void *) producer, (void *) 0) != 0) { 
-		printf("Error creating thread producer! Exiting! \n");
-		exit(0);
-	}
-	if (pthread_create(&cons_handle, 0, (void *) consumer, (void *) 0) != 0) { 
-		printf("Error creating thread consumer! Exiting! \n");
-		exit(0);
+	for(int i = 0; i < N; i++) {
+		if (pthread_create(&prod_handle[i], 0, (void *) producer, (void *) 0) != 0) { 
+			printf("Error creating thread producer! Exiting! \n");
+			exit(0);
+		}
+		if (pthread_create(&cons_handle[i], 0, (void *) consumer, (void *) 0) != 0) { 
+			printf("Error creating thread consumer! Exiting! \n");
+			exit(0);
+		}
 	}
 	
 	printf("\n Thread pai vai esperar filhas.\n\n");
 	fflush(0);
 
-	pthread_join(prod_handle, 0);
-	pthread_join(cons_handle, 0);
-
+	for(int i = 0; i < N; i++) {
+		pthread_join(prod_handle[i], 0);
+		pthread_join(cons_handle[i], 0);
+	}
 	getchar();
 
 	printf("Thread pai saindo.\n");
